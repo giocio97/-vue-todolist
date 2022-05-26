76eb8dfee2b6f41df67d8f616a9545dcb2eeef3f
Giovanni Ciotta <giociotta@gmail.com>
2022-05-26 16:21:07 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const listToDo = new Vue({
+el:"#app",
+data: {
+   
+    toDo:[
+        {
+            text: `fare la spesa`,
+            done: true
+        },
+
+        {
+            text: `pulire casa`,
+            done: true
+        },
+
+        {
+            text: `comprare pane`,
+            done: false
+        },
+
+        {
+            text: `lavare la macchina`,
+            done: true
+        },
+
+        {
+            text: `studiare`,
+            done: false
+        },
+
+
+
+
+    ]
+},
+
+// methods:{
+//     newTask(){
+//         const obj = new task 
+//     }
+// }
+});
